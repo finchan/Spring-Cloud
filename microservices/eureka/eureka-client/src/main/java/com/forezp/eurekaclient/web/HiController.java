1a2623fb0f62b1112419234a88e043a6aa4a7c96
finchan <finchan@outlook.com>
2019-02-14 22:41:22 +0800

    Initialization

--- microservices/eureka/eureka-client/src/main/java/com/forezp/eurekaclient/web/HiController.java
+++ microservices/eureka/eureka-client/src/main/java/com/forezp/eurekaclient/web/HiController.java
@@ -1,6 +1,7 @@
 package com.forezp.eurekaclient.web;
 
 //import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
-//    @Autowired
+    //    @Autowired
 //    Environment environment;
     @Value("${server.port}")
     String port;
+
     @GetMapping("/hi")
     public String home(@RequestParam String name) {
-        return "hi "+name+",i am from port:" +port;
+        return "hi " + name + ",i am from port:" + port;
 //        return "hi "+name+",i am from port:" + environment.getProperty("local.server.port");
     }
 }
