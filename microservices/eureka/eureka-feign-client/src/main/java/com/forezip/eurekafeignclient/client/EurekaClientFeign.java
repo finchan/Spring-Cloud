19888144577df61d8b85b5a3216f242841fd79f5
Xavier Ding <finchan_ding@hotmail.com>
2019-02-19 10:45:08 +0800

    Feign Client

---
+++ microservices/eureka/eureka-feign-client/src/main/java/com/forezip/eurekafeignclient/client/EurekaClientFeign.java
@@ -0,0 +1,12 @@
+package com.forezip.eurekafeignclient.client;
+
+import com.forezip.eurekafeignclient.client.config.FeignConfig;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value="eureka-client", configuration = FeignConfig.class)
+public interface EurekaClientFeign {
+    @GetMapping(value="/hi")
+    String sayHiFromClientEureka(@RequestParam(value="name") String name);
+}
