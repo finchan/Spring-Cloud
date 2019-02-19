deb0378e1934b2d5c43034cea0b6ced1d1bd59ad
finchan <finchan@outlook.com>
2019-02-19 23:18:17 +0800

    Feign Client

---
+++ microservices/eureka/feign-hystrix-dashboard/src/main/java/com/forezip/eurekafeignclient/EurekaFeignClientApplication.java
@@ -0,0 +1,21 @@
+package com.forezip.eurekafeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@SpringBootApplication
+public class EurekaFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignClientApplication.class, args);
+    }
+
+}
---
+++ microservices/eureka/feign-hystrix-dashboard/src/main/java/com/forezip/eurekafeignclient/service/HiService.java
@@ -0,0 +1,14 @@
+package com.forezip.eurekafeignclient.service;
+
+import com.forezip.eurekafeignclient.client.EurekaClientFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HiService {
+    @Autowired
+    EurekaClientFeign eurekaClientFeign;
+    public String sayHi(String name) {
+        return eurekaClientFeign.sayHiFromClientEureka(name);
+    }
+}
