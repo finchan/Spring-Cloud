9e2ed9ef67f7b9c369216b18329b32951546b3df
Xavier Ding <finchan_ding@hotmail.com>
2019-02-21 17:19:53 +0800

    Zuul

---
+++ microservices/eureka/cloud-zuul/src/main/java/com/forezp/cloudzuul/hystrix/MyFallbackProvider.java
@@ -0,0 +1,59 @@
+package com.forezp.cloudzuul.hystrix;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class MyFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        //Hystrix eureka-client application
+        //If you want to hystrix all applications - return "*";
+        return "eureka-client";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("oooops! error, I'm the fallback.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
