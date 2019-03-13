beef5c83e98caf7f305844c3752549b7ff993cdd
Xavier Ding <finchan_ding@hotmail.com>
2019-03-13 17:14:47 +0800

    Sleuth & Zipkin & MySQL & Http

---
+++ microservices/eureka/http-zipkin-server-mysql/src/main/java/com/forezp/sleuth/SleuthZipkinApplication.java
@@ -0,0 +1,26 @@
+package com.forezp.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import zipkin.server.internal.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class SleuthZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthZipkinApplication.class, args);
+    }
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
+
+}
